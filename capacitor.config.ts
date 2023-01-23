92c3aab53586557fe43cc1f026bb5222d563cdc0
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-01-23 22:26:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PMDMO-Ej2_App_cursos_IONIC+Angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
